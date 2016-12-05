b30e7b8da23f3fa764f8af2e1d43a269db574d89
Poonsak <poonsak.nuch@gmail.com>
2016-12-05 15:17:05 +0700

    can run

---
+++ avoidTheLight.py
@@ -0,0 +1,43 @@
+import arcade
+ 
+from models import World, Bat
+
+SCREEN_WIDTH = 450
+SCREEN_HEIGHT = 600
+SCALE = 0.5
+
+SRC = {"bat" : "images/bat.png"}
+ 
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+     
+        super().__init__(*args, **kwargs)
+
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
+
+class AvoidTheLightGameWindow(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+ 
+        arcade.set_background_color(arcade.color.WHITE)
+ 
+        self.world = World(width, height)
+        self.bat_sprite = ModelSprite(SRC['bat'], model=self.world.bat)
+        print(type(self.bat_sprite))
+ 
+    def on_draw(self):
+        arcade.start_render()
+        self.bat_sprite.draw()
+ 
+    def animate(self, delta):
+        self.world.animate(delta)
+if __name__ == '__main__':
+    window = AvoidTheLightGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
+    arcade.run()
---
+++ models.py
@@ -0,0 +1,20 @@
+class Bat:
+    def __init__(self, world, x, y):
+        self.world = world
+        self.x = x
+        self.y = y
+
+    def animate(self, delta):
+        if self.y > self.world.height:
+            self.y = 0
+        self.y += 5
+
+class World:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+        self.bat = Bat(self, int(self.width/2), int(self.height/2))
+
+    def animate(self, delta):
+        self.bat.animate(delta)
