9ef41d870a86e105f4a0d01a92f640d633fd2b98
Poonsak <poonsak.nuch@gmail.com>
2016-12-09 10:54:38 +0700

    add check_for_collision

--- avoidTheLight.py
+++ avoidTheLight.py
@@ -31,7 +31,7 @@ class AvoidTheLightGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
-        arcade.set_background_color((94, 120, 197, 0))
+        arcade.set_background_color((46, 53, 124, 0))
 
         self.world = World(width, height)
         self.bat_sprite = ModelSprite(SRC['bat'], model=self.world.bat)
@@ -42,7 +42,7 @@ class AvoidTheLightGameWindow(arcade.Window):
             n += 1
             self.firefly_sprites.append(ModelSprite(SRC['firefly'], model=firefly))
         
-
+        
     def on_key_press(self, key, key_modifiers):
         self.world.on_key_press(key, key_modifiers)
 
@@ -57,6 +57,19 @@ class AvoidTheLightGameWindow(arcade.Window):
 
     def animate(self, delta):
         self.world.animate(delta)
+        self.check_collision(self.bat_sprite, self.firefly_sprites)
+    
+    def check_collision(self, bat_sprite, firefly_sprites):
+        # collision_list = arcade.check_for_collision_with_list(bat_sprite, firefly_sprites)
+        # print(collision_list)
+        # print(len(collision_list))
+        for sprite in firefly_sprites:
+            is_collided = arcade.check_for_collision(bat_sprite, sprite)
+            print(is_collided)
+            n = 0
+            print(n)
+            n += 1
+        n = 0
 
 if __name__ == '__main__':
     window = AvoidTheLightGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
